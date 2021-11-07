db5183c5f27a9a17a76d99d7771d600f4c2b4ee4
Martin Jimenez Garcia <42785372+marzers42@users.noreply.github.com>
2021-11-07 15:15:57 -0600

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_Xyxy76cCe', // e.g. us-east-2_Xyxy76cCe
+        userPoolClientId: '6sm9k9bmdh0hh2r63jj7v2cmns', // e.g. 6sm9k9bmdh0hh2r63jj7v2cmns
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://jkh4zc4g2k.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://jkh4zc4g2k.execute-api.us-east-1.amazonaws.com/prod,
+    }
+};
